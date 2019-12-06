9a8ef10ccac7a768056b18555733f733df1479e5
Johan Lorenzo <jlorenzo@mozilla.com>
2019-12-06 09:59:08 +0100

    Bug 1599128 - Use taskgraph's "project" to tell forks from main repo

--- taskcluster/fenix_taskgraph/routes.py
+++ taskcluster/fenix_taskgraph/routes.py
@@ -12,9 +12,9 @@ from taskgraph.transforms.task import index_builder
 # In the future, notifying consumers may be easier (https://bugzilla.mozilla.org/show_bug.cgi?id=1548810), but
 # we need to remember to tell users for the time being
 SIGNING_ROUTE_TEMPLATES = [
-    "index.project.{trust-domain}.{project}.v2.{variant}.{build_date}.revision.{head_rev}",
-    "index.project.{trust-domain}.{project}.v2.{variant}.{build_date}.latest",
-    "index.project.{trust-domain}.{project}.v2.{variant}.latest",
+    "index.project.{trust-domain}.{repo_name}.v2.{variant}.{build_date}.revision.{head_rev}",
+    "index.project.{trust-domain}.{repo_name}.v2.{variant}.{build_date}.latest",
+    "index.project.{trust-domain}.{repo_name}.v2.{variant}.latest",
 ]
 
 
@@ -31,6 +31,7 @@ def add_signing_indexes(config, task):
     )
     subs["trust-domain"] = config.graph_config["trust-domain"]
     subs["variant"] = task["attributes"]["build-type"]
+    subs["repo_name"] = config.params["project"].split("/")[-1]
 
     for tpl in SIGNING_ROUTE_TEMPLATES:
         routes.append(tpl.format(**subs))
