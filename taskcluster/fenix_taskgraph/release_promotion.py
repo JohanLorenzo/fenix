eb9284563ad670250cfc127406adc1335be633cd
Johan Lorenzo <jlorenzo@mozilla.com>
2020-11-12 17:46:43 +0100

    fix import

--- taskcluster/fenix_taskgraph/release_promotion.py
+++ taskcluster/fenix_taskgraph/release_promotion.py
@@ -2,9 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import os
-
 from __future__ import absolute_import, print_function, unicode_literals
+
+import os
 
 from mozilla_version.fenix import FenixVersion
 from taskgraph.actions.registry import register_callback_action
